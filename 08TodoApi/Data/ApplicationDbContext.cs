00df2f03421e2cbc29f6a32ca3d7b91dd01bde46
mauriutem6 <mauriutem@gmail.com>
2024-01-26 13:53:38 -0300

    c# .net core 7, sql server crud scaffolding

---
+++ 08TodoApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection.Emit;
+
+namespace _08TodoApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
+        // public DbSet<NombreClase> NombreTablaEnBaseDeDatos { get; set; }
+    }
+}
